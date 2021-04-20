d2671f7ab712dd1c479704118ee6c320f130494c
agarictree <ormhild@gmail.com>
2021-04-20 16:43:01 +0300

    добавление анимации шара

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let result = document.getElementById("result");
 let button = document.getElementById("getAnswer");
+let animationElement = document.getElementById("animation");
+window.onload = function() {
+    let rotate = 0;
+    animationElement.classList.add("animation");
+    setInterval(() => {
+        rotate+=10;
+        animationElement.style.transform = `rotate(${rotate}deg)`
+    }, 60)
+}
 button.onclick = function() {
     let img = result.firstElementChild;
     img.remove();
