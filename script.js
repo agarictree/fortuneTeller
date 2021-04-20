48382f41f2cd354ae3447247ab378d03aaebec26
agarictree <ormhild@gmail.com>
2021-04-20 17:59:14 +0300

    Исправление анимации, задание расположения элементовЖ знак вопроса и анимация

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 let result = document.getElementById("result");
+let answer = document.getElementById("answer");
 let button = document.getElementById("getAnswer");
 let animationElement = document.getElementById("animation");
+
+let coords = answer.getBoundingClientRect();
+console.log(coords);
+
+let resultCoords = result.getBoundingClientRect();
+console.log(resultCoords);
+
+result.style.top = (coords.height / 2) - (resultCoords.height / 2) + "px";
+result.style.left = (coords.width / 2) - (resultCoords.width / 2) + "px";
+
+let animationElementCoords = animationElement.getBoundingClientRect();
+
+animationElement.style.top = (coords.height / 2) - (animationElementCoords.height / 2) + "px";
+animationElement.style.left = (coords.width / 2) - (animationElementCoords.width / 2) + "px"
+
+window.onresize = () => {
+    let coords = answer.getBoundingClientRect();
+    let resultCoords = result.getBoundingClientRect();
+    result.style.top = (coords.height / 2) - (resultCoords.height / 2) + "px";
+    result.style.left = (coords.width / 2) - (resultCoords.width / 2) + "px";
+
+    animationElement.style.top = (coords.height / 2) - (animationElementCoords.height / 2) + "px";
+    animationElement.style.left = (coords.width / 2) - (animationElementCoords.width / 2) + "px"
+}
+
 window.onload = function() {
     let rotate = 0;
     animationElement.classList.add("animation");
@@ -9,12 +35,16 @@ window.onload = function() {
         animationElement.style.transform = `rotate(${rotate}deg)`
     }, 60)
 }
+
 button.onclick = function() {
     let img = result.firstElementChild;
     img.remove();
     let answers = ["Да", "Нет"];
     let num = Math.round(Math.random());
     let total = answers[num];
-    result.textContent = total;
+    let textBlock = document.createElement("div");
+    textBlock.textContent = total;
+    result.append(textBlock);
+    textBlock.id = "answerText";
     console.log(num);
 }
