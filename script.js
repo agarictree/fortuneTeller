15165b03fc1404070be240b8606d9400f9b04526
agarictree <ormhild@gmail.com>
2021-08-30 16:43:57 +0300

    добавление резиновой и адаптивной верстки, замена js анимации на keyframes

--- script.js
+++ script.js
@@ -1,41 +1,8 @@
 let result = document.getElementById("result");
 let answer = document.getElementById("answer");
 let button = document.getElementById("getAnswer");
+let container = document.querySelector(".container");
 let animationElement = document.getElementById("animation");
-
-let coords = answer.getBoundingClientRect();
-console.log(coords);
-
-let resultCoords = result.getBoundingClientRect();
-console.log(resultCoords);
-
-result.style.top = (coords.height / 2) - (resultCoords.height / 2) + "px";
-result.style.left = (coords.width / 2) - (resultCoords.width / 2) + "px";
-
-let animationElementCoords = animationElement.getBoundingClientRect();
-
-animationElement.style.top = (coords.height / 2) - (animationElementCoords.height / 2) + "px";
-animationElement.style.left = (coords.width / 2) - (animationElementCoords.width / 2) + "px"
-
-window.onresize = () => {
-    let coords = answer.getBoundingClientRect();
-    let resultCoords = result.getBoundingClientRect();
-    result.style.top = (coords.height / 2) - (resultCoords.height / 2) + "px";
-    result.style.left = (coords.width / 2) - (resultCoords.width / 2) + "px";
-
-    animationElement.style.top = (coords.height / 2) - (animationElementCoords.height / 2) + "px";
-    animationElement.style.left = (coords.width / 2) - (animationElementCoords.width / 2) + "px"
-}
-
-window.onload = function() {
-    let rotate = 0;
-    animationElement.classList.add("animation");
-    setInterval(() => {
-        rotate+=10;
-        // if(rotate == 360) rotate = 0;
-        animationElement.style.transform = `rotate(${rotate}deg)`
-    }, 60);
-}
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
@@ -57,9 +24,6 @@ button.onclick = function() {
     textBlock.id = "answerText";
     setTimeout(() => {
         textBlock.remove();
-        // let img = document.createElement("img");
-        // img.src = "images/03.png";
-        // img.id = "getAnswer";
         result.append(img);
-    }, 1000);
+    }, 10000);
 }
